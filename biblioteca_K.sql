382d740a4e10216b909ba061043357acb2ea3a79
bibi-santos <gabrielasantos211200@gmail.com>
2026-09-22 11:53:59 -0300

    Add files via upload

---
+++ biblioteca_K.sql
@@ -0,0 +1,35 @@
+/*create database gabi_turma_k;*/
+/*create table funcionarios
+ (
+   codfunc int not null auto_increment,
+   CPF char(15) not null,
+   nome varchar(45) not null,
+   salario varchar(45) not null,
+   funcao varchar(45) not null,
+   primary key(codfunc)
+ );*/
+ 
+ /*create table editoras
+  (
+    coded int not null auto_increment,
+    nome varchar(45) not null,
+    contato varchar(45) not null,
+    primary key(coded)
+  );*/
+  
+ /*create table autores
+  (
+    codaut int not null auto_increment,
+    nome varchar(45) not null,
+    nacionalidade varchar(45) not null,
+    primary key(codaut)
+  );*/
+  
+ /*create table usuarios 
+  (
+    codus int not null auto_increment,
+    cpf char(45) not null,
+    nome varchar(45) not null,
+    primary key(codus)
+  );*/
+ 
