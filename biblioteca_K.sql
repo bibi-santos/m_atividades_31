afe34ffbf074c49392e312c1b51bff6718718e02
bibi-santos <gabrielasantos211200@gmail.com>
2026-09-24 09:47:52 -0300

    Add files via upload

--- biblioteca_K.sql
+++ biblioteca_K.sql
@@ -32,4 +32,23 @@
     nome varchar(45) not null,
     primary key(codus)
   );*/
+  
+create table livros
+  (
+    codliv int not null auto_increment,
+    numero int not null,
+    titulo varchar(100) not null,
+    genero varchar(45) not null,
+    edicao varchar(5) not null,
+    ano_publi smallint(4) not null,
+    codus_fk int not null,
+    coded_fk int not null,
+    codfunc_fk int not null,
+    primary key(codliv),
+    foreign key(codus_fk) references usuarios(codus),
+    foreign key(coded_fk) references editoras(coded),
+    foreign key(codfunc_fk) references funcionarios(codfunc)
+  );
+  
+
  
